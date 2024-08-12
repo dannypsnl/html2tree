6ee492fd6b680a73d2e5cee0615026dc159b6f81
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-08-12 22:36:08 +0800

    (feat) convert HTML input to tree output
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 import van from "vanjs-core";
+import * as htmlparser2 from "htmlparser2";
 
 const { p, div, pre, input } = van.tags;
 
-const domParser = new DOMParser();
+const html2tree = (str) => {
+  let res = ``;
+  const parser = new htmlparser2.Parser({
+    onopentagname: (name) => (res += `\\<html:${name}>`),
+    onattribute: (name, value) => (res += `[${name}]{${value}}`),
+    onopentag: (_name, _attrs) => (res += `{`),
+    ontext: (text) => (res += text),
+    onclosetag: (_) => (res += `}`),
+  });
+
+  parser.write(str);
+  parser.end();
+  return res;
+};
 
 const App = () => {
-  const html_input = van.state("");
+  const html_input =
+    van.state(`<blockquote lang="zh" cite="https://g0v.social/@dannypsnl/112946647273534525">
+  <p>schema 在軟體端的型別都讓人有點失望，直接做成</p><p>table Book<br />  name : string<br />  isbn : string       add(1)<br />  url: string           remove(2)</p><p>標記對欄位的修正就好了</p>
+  <footer>
+     — Lîm Tsú-thuàn (@dannypsnl) <a href="https://g0v.social/@dannypsnl/112946647273534525"><time datetime="2024-08-12T02:17:46.064Z">8/12/2024, 10:17:46 AM</time></a>
+  </footer>
+</blockquote>`);
+  const output_tree = van.state(``);
 
   van.derive(() => {
-    let res = domParser.parseFromString(html_input.val, "text/html");
-    console.log(res);
+    output_tree.val = html2tree(html_input.val);
   });
 
   return div(
@@ -19,7 +39,7 @@ const App = () => {
       oninput: (e) => (html_input.val = e.target.value),
     }),
     pre("output part:"),
-    p(html_input)
+    p(output_tree)
   );
 };
 
