3eabbde652b67660326dd32a32f821a1ef0c18c8
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-08-12 21:43:07 +0800

    basic reactive setup
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 import van from "vanjs-core";
 
-const { button, div, pre, input } = van.tags;
+const { p, div, pre, input } = van.tags;
+
+const domParser = new DOMParser();
 
 const App = () => {
+  const html_input = van.state("");
+
+  van.derive(() => {
+    let res = domParser.parseFromString(html_input.val, "text/html");
+    console.log(res);
+  });
+
   return div(
-    input({ type: "text", value: "", onInput: function () {} }),
-    pre("output part")
+    input({
+      type: "text",
+      value: html_input,
+      oninput: (e) => (html_input.val = e.target.value),
+    }),
+    pre("output part:"),
+    p(html_input)
   );
 };
 
