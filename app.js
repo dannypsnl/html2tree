b61922adf742b6a754aed99ac238717036ae4f7c
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-08-13 14:48:45 +0800

    add checkbox to cancel inserted namespace line
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import van from "vanjs-core";
 import * as htmlparser2 from "htmlparser2";
 import { svg as example } from "./example";
 
-const { h1, code, button, div, pre, textarea, p } = van.tags;
+const { h1, code, button, div, pre, input, textarea, p, label } = van.tags;
 
 const html2tree = (str) => {
   let res = ``;
@@ -21,9 +21,10 @@ const html2tree = (str) => {
 
 const App = () => {
   const html_input = van.state(example);
+  const add_xmlns = van.state(true);
   const output_tree = van.derive(
     () =>
-      `\\xmlns:html{http://www.w3.org/1999/xhtml}\n\n` +
+      (add_xmlns.val ? `\\xmlns:html{http://www.w3.org/1999/xhtml}\n\n` : ``) +
       html2tree(html_input.val)
   );
 
@@ -32,6 +33,14 @@ const App = () => {
     p(
       `Put HTML into the textarea, then copy the forester tree right hand side.`
     ),
+    input({
+      type: `checkbox`,
+      id: `add-namespace`,
+      onclick: () => {
+        add_xmlns.val = !add_xmlns.val;
+      },
+    }),
+    label({ for: `add-namespace` }, `disable inserted namespace`),
     div(
       textarea({
         value: html_input,
@@ -43,10 +52,10 @@ const App = () => {
           {
             onclick: () => {
               navigator.clipboard.writeText(output_tree.val);
-              alert("copied!");
+              alert(`copied!`);
             },
           },
-          "copy"
+          `copy`
         )
       )
     )
